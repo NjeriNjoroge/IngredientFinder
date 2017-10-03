ba019efe6d6c430608da2364259bed66d38ede5d
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-10-03 10:21:14 +0300

    initial commit. created a model for the API call, included internet access in android manifest

---
+++ app/src/main/java/com/example/gnjoroge/ingredientfinder/Constants.java
@@ -0,0 +1,10 @@
+package com.example.gnjoroge.ingredientfinder;
+
+/**
+ * Created by gnjoroge on 10/3/17.
+ */
+
+public class Constants {
+
+    public static final String PUPPY_BASE_URL = "http://www.recipepuppy.com/api/?q=";
+}
---
+++ app/src/main/java/com/example/gnjoroge/ingredientfinder/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gnjoroge.ingredientfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    }
